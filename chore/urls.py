182d5e78f33829b186a55f4d650db1fe4066eb9c
Mehedi Hasan <git.mehedi@gmail.com>
2015-11-02 10:34:27 +0600

    [CONFLICT] Resolve conflict

--- chore/urls.py
+++ chore/urls.py
@@ -1,7 +1,10 @@
-from . import views
 from django.conf.urls import url
+
+from . import views
+
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^(?P<chorelist_id>[0-9]+)$', views.detail, name="detail"),
 ]
 
--- chore/views.py
+++ chore/views.py
@@ -3,22 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse, Http404
-from . import models
+from .models import Chorelist
 
 
 def index(request):
-    lists = models.Chorelist.objects.all()
+    lists = Chorelist.objects.all()
+    context = {'chorelists': lists}
     return render(request,'chores/index.html',context)
 
-def details(request,chorelist_id):
-    try:
-        list = models.Chorelist.objects.get(pk=chorelist_id)
-    except Chorelist.DoesNotExist:
-        raise Http404('Chorelist does not exists')
-    return render(request, 'chores/details.html',{'chorelist':list})
+def detail(request,chorelist_id):
+    chorelist = Chorelist.objects.get(pk=chorelist_id)
+    context = {'chorelist': chorelist}
+    return render(request,'chores/detail.html',context )
 
-def chore(request,chore_id):
-    return HttpResponse('This is your first chore_id#%s'% chore_id)
-
-def chorelist(request,chorelist_id,chore_id):
-    return HttpResponse('This is your first chorelist#%s of chore#5s'% (chorelist_id,chore_id))
+#def details(request,chorelist_id):
+#    try:
+#        list = models.Chorelist.objects.get(pk=chorelist_id)
+#    except Chorelist.DoesNotExist:
+#        raise Http404('Chorelist does not exists')
+#    return render(request, 'chores/details.html',{'chorelist':list})
+#
+#def chore(request,chore_id):
+#    return HttpResponse('This is your first chore_id#%s'% chore_id)
+#
+#def chorelist(request,chorelist_id,chore_id):
+#    return HttpResponse('This is your first chorelist#%s of chore#5s'% (chorelist_id,chore_id))
